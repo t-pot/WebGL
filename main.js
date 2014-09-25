dd0113f4e51b5d808b08cf006e76fc8e8824c08e
Takashi Imagire <imagire@gmail.com>
2014-09-25 17:13:04 +0900

    test for debug

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ onload = function(){
 	
 	(function(){
 		// フレームバッファの変更
-		gl.bindFramebuffer(gl.FRAMEBUFFER, fBuffer.f);
+//		gl.bindFramebuffer(gl.FRAMEBUFFER, fBuffer.f);
 
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clearDepth(1.0);
@@ -100,21 +100,21 @@ onload = function(){
 		gl.drawElements(gl.TRIANGLES, 3*2328, gl.UNSIGNED_SHORT, 0);
 		
 		// 元のフレームバッファに戻す
-		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+//		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 		
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		gl.clearDepth(1.0);
-		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+//		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+//		gl.clearDepth(1.0);
+//		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		
 		// 画面全体を覆うスクリーンを描いてトーンマッピング
-		tpotEngine.set_attribute(vbo_tonemap, gl, prg_tonemap, SHADER_TYPE.TONEMAP);
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo_tonemap);
+//		tpotEngine.set_attribute(vbo_tonemap, gl, prg_tonemap, SHADER_TYPE.TONEMAP);
+//		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo_tonemap);
 
-		gl.activeTexture(gl.TEXTURE0);
-		gl.bindTexture(gl.TEXTURE_2D, fBuffer.t);
-		gl.uniform1i(uniLocation_tonemap[0], 0);
+//		gl.activeTexture(gl.TEXTURE0);
+//		gl.bindTexture(gl.TEXTURE_2D, fBuffer.t);
+//		gl.uniform1i(uniLocation_tonemap[0], 0);
 		
-		gl.drawElements(gl.TRIANGLES, 3*2, gl.UNSIGNED_SHORT, 0);
+//		gl.drawElements(gl.TRIANGLES, 3*2, gl.UNSIGNED_SHORT, 0);
 
 		// 描画更新
 		gl.flush();
