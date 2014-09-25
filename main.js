006d6500165d9dc1158b59004cd462ecb279e354
Takashi Imagire <imagire@gmail.com>
2014-09-25 12:06:15 +0900

    1st commit

---
+++ main.js
@@ -0,0 +1,127 @@
+onload = function(){
+
+	var gl = tpotEngine.getContext(512, 512);
+	
+	// シーン描画用のオブジェクト
+	var prg = tpotEngine.get_program(SHADER_TYPE.PHONG, gl);
+	
+	// VBOの生成
+	var vbo = tpotEngine.create_vbo(teapot_vtx_xnt, gl);
+	var ibo = tpotEngine.create_ibo(teapot_index, gl);
+	var tex = tpotEngine.createTexture('t-pot.png', gl);
+	
+	// uniformLocationの取得
+	var uniLocation = new Array();
+	uniLocation[0] = gl.getUniformLocation(prg, 'mvpMatrix');
+	uniLocation[1] = gl.getUniformLocation(prg, 'invMatrix');
+	uniLocation[2] = gl.getUniformLocation(prg, 'lightDirection');
+	uniLocation[3] = gl.getUniformLocation(prg, 'eyeDirection');
+	uniLocation[4] = gl.getUniformLocation(prg, 'ambientColor');
+	uniLocation[5] = gl.getUniformLocation(prg, 'texture');
+
+	// トーンマッピング用のオブジェクト初期化
+	var ext;
+	ext = gl.getExtension('OES_texture_float');
+	if(ext == null){
+		alert('float texture not supported');
+		return;
+	}
+	
+	var prg_tonemap = tpotEngine.get_program(SHADER_TYPE.TONEMAP, gl);
+	var vbo_tonemap = tpotEngine.create_vbo(
+		[-1,-1,0.5, 0,0,
+		 +1,-1,0.5, 1,0,
+		 -1,+1,0.5, 0,1,
+		 +1,+1,0.5, 1,1], 
+		gl);
+	var ibo_tonemap = tpotEngine.create_ibo(
+		[0,1,2, 1,3,2], 
+		gl);
+	var fBuffer = tpotEngine.create_framebuffer(0, 0, gl);
+	
+	var uniLocation_tonemap = new Array();
+	uniLocation_tonemap[0] = gl.getUniformLocation(prg_tonemap, 'texture');
+	
+	// 各種行列の生成と初期化
+	var mMatrix = tpotMat.identity();
+	var vMatrix = tpotMat.identity();
+	var pMatrix = tpotMat.identity();
+	var tmpMatrix = tpotMat.identity();
+	var mvpMatrix = tpotMat.identity();
+	var invMatrix = tpotMat.identity();
+	
+	var lightDirection = [1.5, 1.0, 0.5];		// 平行光源の向き
+	var ambientColor = [0.1, 0.1, 0.1, 1.0];	// 環境光の色
+	var eyeDirection = [0.0, 1.0, 5.0];			// 視点ベクトル
+	
+	// 変換行列
+	tpotMat.lookAt(eyeDirection, [1, -1, 0], [0, 1, 0], vMatrix);
+	tpotMat.perspective(45, tpotEngine.screen_width() / tpotEngine.screen_height(), 0.1, 100, pMatrix);
+	tpotMat.multiply(pMatrix, vMatrix, tmpMatrix);
+	
+	// 状態初期化
+	gl.enable(gl.DEPTH_TEST);
+	gl.depthFunc(gl.LEQUAL);
+	gl.enable(gl.CULL_FACE);
+	
+	// カウンタの宣言
+	var frames = 0;
+	
+	(function(){
+		// フレームバッファの変更
+		gl.bindFramebuffer(gl.FRAMEBUFFER, fBuffer.f);
+
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearDepth(1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		
+		// シーンの描画
+		var rad = (frames % 360) * Math.PI / 180;
+		
+		tpotEngine.set_attribute(vbo, gl, prg, SHADER_TYPE.PHONG);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, tex);
+		
+		mMatrix = tpotMat.identity();
+		tpotMat.translate(mMatrix, [1.0, -1.0, 0.0], mMatrix);
+		tpotMat.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+
+		tpotMat.multiply(tmpMatrix, mMatrix, mvpMatrix);
+		tpotMat.inverse(mMatrix, invMatrix);
+
+		gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+		gl.uniformMatrix4fv(uniLocation[1], false, invMatrix);
+		gl.uniform3fv(uniLocation[2], lightDirection);
+		gl.uniform3fv(uniLocation[3], eyeDirection);
+		gl.uniform4fv(uniLocation[4], ambientColor);
+		gl.uniform1i(uniLocation[5], 0);
+
+		gl.drawElements(gl.TRIANGLES, 3*2328, gl.UNSIGNED_SHORT, 0);
+		
+		// 元のフレームバッファに戻す
+		gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+		
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearDepth(1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		
+		// 画面全体を覆うスクリーンを描いてトーンマッピング
+		tpotEngine.set_attribute(vbo_tonemap, gl, prg_tonemap, SHADER_TYPE.TONEMAP);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo_tonemap);
+
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, fBuffer.t);
+		gl.uniform1i(uniLocation_tonemap[0], 0);
+		
+		gl.drawElements(gl.TRIANGLES, 3*2, gl.UNSIGNED_SHORT, 0);
+
+		// 描画更新
+		gl.flush();
+		
+		// 更新を待つ
+		frames++;
+		setTimeout(arguments.callee, 1000 / 60);
+	})();
+};
+
---
+++ tpotEngine.js
@@ -0,0 +1,228 @@
+var SHADER_TYPE = {
+	CONST: 0,
+	DIFFUSE: 1,
+	PHONG: 2,
+	TONEMAP: 3,
+}
+
+var tpotEngine = function(){
+
+	var screen_size = [640, 480];
+
+	var create_shader = function(id, gl)
+	{
+		var shader;
+		var scriptElement = document.getElementById(id);
+		
+		if(!scriptElement){return;}
+		
+		switch(scriptElement.type){
+			
+			case 'x-shader/x-vertex':
+				shader = gl.createShader(gl.VERTEX_SHADER);
+				break;
+				
+			case 'x-shader/x-fragment':
+				shader = gl.createShader(gl.FRAGMENT_SHADER);
+				break;
+			default :
+				return;
+		}
+		
+		gl.shaderSource(shader, scriptElement.text);
+		gl.compileShader(shader);
+		
+		if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+			alert(gl.getShaderInfoLog(shader));
+		}
+
+		return shader;
+	}
+	
+	var create_program = function(vs, fs, gl){
+		var program = gl.createProgram();
+		
+		gl.attachShader(program, vs);
+		gl.attachShader(program, fs);
+		gl.linkProgram(program);
+		
+		if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
+			alert(gl.getProgramInfoLog(program));
+		}
+
+		gl.useProgram(program);
+		return program;
+	}
+	return {
+		getContext: function(width, height)
+		{
+			var c = document.getElementById('canvas');
+			c.width = screen_size[0] = width;
+			c.height = screen_size[1] = height;
+
+			return c.getContext('webgl') || c.getContext('experimental-webgl');
+		},
+
+		get_program: function(shader_type, gl)
+		{
+			var vs_name, fs_name;
+			
+			switch(shader_type){
+				
+				case SHADER_TYPE.CONST:
+					vs_name = 'Const_vs';
+					fs_name = 'Const_fs';
+					break;
+					
+				case SHADER_TYPE.DIFFUSE:
+					vs_name = 'Diffuse_vs';
+					fs_name = 'Diffuse_fs';
+					break;
+
+				case SHADER_TYPE.PHONG:
+					vs_name = 'Phong_vs';
+					fs_name = 'Phong_fs';
+					break;
+
+				case SHADER_TYPE.TONEMAP:
+					vs_name = 'Tonemap_vs';
+					fs_name = 'Tonemapt_fs';
+					break;
+
+				default :
+					return;
+			}
+
+			return create_program(
+				create_shader(vs_name, gl), 
+				create_shader(fs_name, gl), 
+				gl);
+		},
+
+		create_vbo: function(data, gl){
+			var vbo = gl.createBuffer();
+			
+			gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ARRAY_BUFFER, null);
+			
+			return vbo;
+		},
+
+		create_ibo: function(data, gl){
+			var ibo = gl.createBuffer();
+			
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+			
+			return ibo;
+		},
+
+		create_framebuffer: function(width, height, gl){
+			if(width == 0){
+				width = screen_size[0];
+				height = screen_size[1];
+			}
+		
+			var frameBuffer = gl.createFramebuffer();
+			gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
+			
+			// 深度バッファ用レンダーバッファの生成とバインド
+			var depthRenderBuffer = gl.createRenderbuffer();
+			gl.bindRenderbuffer(gl.RENDERBUFFER, depthRenderBuffer);
+			gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
+			gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthRenderBuffer);
+			
+			// フレームバッファ用テクスチャの生成
+			var fTexture = gl.createTexture();
+			gl.bindTexture(gl.TEXTURE_2D, fTexture);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fTexture, 0);
+			
+			gl.bindTexture(gl.TEXTURE_2D, null);
+			gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+			gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+			
+			return {f : frameBuffer, d : depthRenderBuffer, t : fTexture};
+		},
+		
+		set_attribute: function(vbo, gl, prg, shader_type){
+
+			gl.useProgram(prg);
+			
+			var nameLocation;
+			var attStride;
+			switch(shader_type){
+				
+				case SHADER_TYPE.CONST:
+					nameLocation = ['position', 'color'];
+					attStride = [3, 4];
+					break;
+					
+				case SHADER_TYPE.DIFFUSE:
+					nameLocation = ['position', 'normal', 'uv'];
+					attStride = [3, 3, 2];
+					break;
+
+				case SHADER_TYPE.PHONG:
+					nameLocation = ['position', 'normal', 'uv'];
+					attStride = [3, 3, 2];
+					break;
+
+				case SHADER_TYPE.TONEMAP:
+					nameLocation = ['position', 'uv'];
+					attStride = [3, 2];
+
+					break;
+
+				default :
+					return;
+			}
+
+			gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+			var attLocation = new Array(nameLocation.length);
+			var StrideSum = 0;
+			for(var i in nameLocation){
+				attLocation[i] = gl.getAttribLocation(prg, nameLocation[i]);
+				StrideSum += attStride[i];
+			}
+
+			var offset_attS = 0;
+			for(var i in attLocation){
+				gl.enableVertexAttribArray(attLocation[i]);
+				gl.vertexAttribPointer(attLocation[i], attStride[i], gl.FLOAT, false, 4 * StrideSum, 4 * offset_attS);
+				offset_attS += attStride[i];
+			}
+		},
+		
+		createTexture: function(filename, gl)
+		{
+			var tex = gl.createTexture();
+			tex.image = new Image();
+
+			tex.image.onload = function(){
+				gl.bindTexture(gl.TEXTURE_2D, tex);
+				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, tex.image);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+				gl.generateMipmap(gl.TEXTURE_2D);
+				gl.bindTexture(gl.TEXTURE_2D, null);
+				texture = tex;
+			};
+			
+			tex.image.src = filename;
+			
+			return tex;
+		},
+		
+		screen_width: function(){return screen_size[0];},
+		screen_height: function(){return screen_size[1];}
+	};
+}();
+
